928bc0c3012473b88dedee56a2be08da80c9a68c
alibaduqail <114680099+daddyquail@users.noreply.github.com>
2023-10-07 13:46:29 -0400

    base64 to image

--- api/app.py
+++ api/app.py
@@ -1,6 +1,8 @@
+import base64
 from flask import Flask, request
 from flask_cors import CORS
 from PIL import Image
+from io import BytesIO
 
 app = Flask(__name__)
 
@@ -12,6 +14,14 @@ def crop_image(imgPath, x1, y1, x2, y2):
     img = Image.open(imgPath)
     croppedImg = img.crop((x1, y1, x2, y2))
     return croppedImg
+
+
+def base64_img(base64_string):
+    img_data = base64.b64decode(base64_string)
+    img_bytes = BytesIO(img_data)
+    img = Image.open(img_bytes)
+    return img
+
 
 @app.route("/add", methods=['POST'])
 def add():
